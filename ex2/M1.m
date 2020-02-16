677629ef99ed240b4f0ef72bf53f1d5480925fe8
tianzerun <tian.ze@husky.neu.edu>
2020-02-16 18:04:57 -0500

    finish M1-2

--- ex2/M1.m
+++ ex2/M1.m
@@ -5,5 +5,13 @@
 %               all within joint limits
 
 function qs = M1(q_min, q_max, num_samples)
-
+    qs = zeros(num_samples, 4);
+    
+    for i = 1:4
+        q_i_min = q_min(i);
+        q_i_max = q_max(i);
+        % To generate a number from interval r = a + (b-a).*rand(N,1)
+        % where a is the lower bound and b is the upper bound.
+        qs(:,i) = q_i_min + (q_i_max - q_i_min) * rand(num_samples, 1);
+    end
 end
--- ex2/M2.m
+++ ex2/M2.m
@@ -21,5 +21,51 @@
 %                      symmetric: adjacency(i,j) == adjacency(j,i)
 
 function [samples, adjacency] = M2(robot, q_min, q_max, num_samples, num_neighbors, link_radius, sphere_centers, sphere_radii)
+    samples = zeros(num_samples, 4);
+    samples_count = 0;
+    adjacency = zeros(num_samples, num_samples);
+    
+    % Find num_samples collision free samples.
+    while samples_count < num_samples
+        tmp_samples = M1(q_min, q_max, 20);
+        for i = size(tmp_samples, 1)
+            q = tmp_samples(i,:);
+            if validate_sample(robot, q, q_min, q_max, link_radius, sphere_centers, sphere_radii)
+                samples_count = samples_count + 1;
+                samples(samples_count,:) = q;
+            end
+        end
+    end
+    
+    % Check if near points can be connected.
+    for i = 1:num_samples
+        q = samples(i,:);
+        % Find nearest num_neighbors of q.
+        N_q = find_nearest_neighbors(num_neighbors, i, samples); 
+        
+        for j = 1:size(N_q, 1)
+            n_q_row_num = N_q(j);
+            n_q = samples(n_q_row_num,:);
+            if ~check_edge(robot, q, n_q, link_radius, sphere_centers, sphere_radii)
+                adjacency(i, n_q_row_num) = 1;
+                adjacency(n_q_row_num, i) = 1;
+            end
+        end
+    end
+end
 
+
+function neighbors = find_nearest_neighbors(k, p_index, points)
+    this_p = points(p_index,:);
+    distances = vecnorm(points - this_p, 2, 2);
+    [~, I] = sort(distances);
+    neighbors = I(1+1:k+1,:);
+end
+
+function is_valid = validate_sample(robot, q, q_min, q_max, link_r, sphere_c, sphere_r)
+    is_valid = ~(...
+        any(q < q_min)... 
+        | any(q > q_max)... 
+        | check_collision(robot, q, link_r, sphere_c, sphere_r)...
+    );
 end
